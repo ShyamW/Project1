2bac987b3e1e407649aa137043595d2b18fda2a8
Shyam Thiagarajan <shyam.thiagarajan@gmail.com>
2018-01-20 19:29:02 -0500

    Added Proj

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [System.STAThread]
+        static void Main(string[] args)
+        {   
+            // read clipboard text
+            string a = Clipboard.GetText(); 
+            
+            // remove punctuation and spaces from words
+            var rawWords = Regex.Replace(a, "[!.,;:?\n\r]", " ");
+            List<string> words = rawWords.Split(" ".ToCharArray()).ToList();
+            words = words.Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+
+            // lowercase all the words
+            words = words.ConvertAll(d => d.ToLower());
+
+            // drop duplicate words and sort alphabetically
+            words = words.Distinct().ToList();
+            words = words.OrderBy(q => q).ToList();
+
+            // print to screen
+            words.ForEach(Console.WriteLine);
+
+            Console.Read();
+        }
+    }
+}
